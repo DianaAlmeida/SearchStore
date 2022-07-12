d67e0d7af9addadf02889064fa599f2a1d92d17d
Diana Almeida <dsgalmeida@gmail.com>
2022-07-12 15:30:26 +0100

    +Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Diana Almeida on 12/07/2022.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
