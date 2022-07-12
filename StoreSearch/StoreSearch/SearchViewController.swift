2d1e10243375abaf7b73f3498dda0012601803a7
Diana Almeida <dsgalmeida@gmail.com>
2022-07-12 13:06:07 +0100

    *Change: Rename ViewController to SearchViewController

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Diana Almeida on 12/07/2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
