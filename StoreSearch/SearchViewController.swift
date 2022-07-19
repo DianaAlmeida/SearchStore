75b0755c046b1d46bcb45cf5b3658c7d00ed87a6
Diana Almeida <dsgalmeida@gmail.com>
2022-07-19 17:59:42 +0100

    *Change - Download the artwork

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -42,7 +42,6 @@ class SearchViewController: UIViewController {
         searchBar.becomeFirstResponder()
     }
     
-    
     @IBAction func segmentChanged(_ sender: UISegmentedControl) {
         performSearch()
     }
@@ -176,16 +175,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
             
             let searchResult = searchResults[indexPath.row]
-            cell.nameLabel.text = searchResult.name
-            
-            if searchResult.artist.isEmpty {
-                cell.artistNameLabel.text = "Unknown"
-            } else {
-                cell.artistNameLabel.text = String(
-                    format: "%@ (%@)",
-                    searchResult.artist,
-                    searchResult.type)
-            }
+            cell.configure(for: searchResult)
             
             return cell
         }
