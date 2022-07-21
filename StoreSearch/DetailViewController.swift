f8356567c1b8cb82f2ed49733202c3971db534b0
Diana Almeida <dsgalmeida@gmail.com>
2022-07-21 15:47:49 +0100

    +Add - Animations pop-up

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -19,6 +19,11 @@ class DetailViewController: UIViewController {
     
     var searchResult: SearchResult!
     var downloadTask:URLSessionDownloadTask?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        transitioningDelegate = self
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -95,5 +100,15 @@ class DetailViewController: UIViewController {
 extension DetailViewController: UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
         return (touch.view === self.view)
+    }
+}
+
+extension DetailViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideOutAnimationController()
     }
 }
